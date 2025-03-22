a0120d2ff13f6e0c19e57778dc6b442349c66211
Oliva Druyer <164903068+OlivaDruyer@users.noreply.github.com>
2025-03-22 09:37:21 +0100

    Update database.js

--- app/database.js
+++ app/database.js
@@ -1,7 +1,20 @@
-const pg = require('pg');
+// const pg = require('pg');
 
-const client = new pg.Client(process.env.PG_URL);
+// const client = new pg.Client(process.env.PG_URL);
 
-client.connect(),
+// client.connect(),
 
-module.exports = client;
+// module.exports = client;
+
+require('dotenv').config();
+const { Pool } = require('pg');
+
+// Configuration du pool de connexion
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = pool;
