547f477087fe2539d7b42efa866b89bc9f587946
Oliva <olivadruyer34@gmail.com>
2025-03-22 09:20:06 +0100

    a

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-
 // Charger les variables d'environnement
-require('dotenv').config();
-
+require("dotenv").config();
 
 const sessionMiddleware = require("./app/middlewares/sessionMiddleware.js");
 
 // Importe les dépendances tiers
-const express = require('express');
+const express = require("express");
 
 // Importe les dépendances locales
-const router = require('./router');
+const router = require("./router");
 
 // Creation d'une application Express
-const app = express();  
+const app = express();
 
 // Middleware pour parser les données URL-encoded
 app.use(express.urlencoded({ extended: true }));
@@ -20,17 +18,18 @@ app.use(express.json()); // Ajout pour parser les données JSON
 
 app.use(sessionMiddleware);
 
-
 // Mise en place du moteur de rendu
+//Moteur de template=EJS
 app.set("view engine", "ejs");
+
+//où se situe les fichiers template
 app.set("views", "./app/views");
 
 // Service d'un dossier "static"
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Configuration des routes de notre application
 app.use(router);
-
 
 // On démarre un serveur HTTP
 const PORT = process.env.PORT || 4500;
